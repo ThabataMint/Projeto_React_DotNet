742892da1bdb8d40c4358948e73568e9160f57d9
ThabataMint <thabata.holiver@gmail.com>
2022-08-15 16:33:51 -0300

    Projeto React

---
+++ Front_End/pro-atividade-app/src/App.js
@@ -0,0 +1,83 @@
+
+import { useState } from 'react';
+import './App.css';
+import AtividadeForm from './components/AtividadeForm';
+
+import AtividadeLista from './components/AtividadeLista';
+
+
+let initialState = [
+  {
+     id:1,
+     prioridade:'1',
+     titulo:'título',
+     descricao:'Primeira atividade',
+  },
+  {
+     id:2,
+     prioridade:'2',
+     titulo:'título',
+     descricao:'Segunda atividade',
+  },
+
+
+]
+
+function App() {
+  const [atividades, setAtividades] = useState(initialState);
+  const [atividade, setAtividade] = useState({});
+
+    function addAtividade(e){
+      e.preventDefault();
+    
+
+      const atividade = {
+           id: document.getElementById('id').value,
+           prioridade: document.getElementById('prioridade').value,
+           titulo: document.getElementById('titulo').value,
+           descricao: document.getElementById('descricao').value,
+    
+      };
+     
+     setAtividades([...atividades, {...atividade}]);
+
+  }
+
+  function deletarAtividade(id){
+    const atividadesFiltradas = atividades.filter(
+      (atividade) => atividade.id !== id
+      );
+    setAtividades([...atividadesFiltradas]);
+  
+  }
+
+  function pegarAtividade(id){
+    const atividade = atividades.filter((atividade) => atividade.id === id);
+    setAtividade(atividade[0])
+  }
+
+  return (
+    <>
+      <AtividadeForm
+        addAtividade={addAtividade}
+        ativSelecionada={atividade}
+        atividades={atividades}
+       />
+       
+      <AtividadeLista
+          atividades={atividades}
+          deletarAtividade={deletarAtividade}
+          pegarAtividade={pegarAtividade}
+      />
+</>
+  );
+ }
+               
+            
+                        
+                    
+
+
+export default App;
+                    
+                        
